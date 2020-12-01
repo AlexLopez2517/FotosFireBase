1be5eaa7ddc9ff531441c1ad6b277be4abe76a26
Alex Lopez <alex2761086444@gmail.com>
2020-12-01 12:55:18 -0600

    Validaciones del archivo desde la directiva

--- fotosFirebase-master/src/app/directives/ng-drop-files.directives.ts
+++ fotosFirebase-master/src/app/directives/ng-drop-files.directives.ts
@@ -1,12 +1,14 @@
 import { EventEmitter } from '@angular/core';
 import { Directive, EventEmitter, ElementRef,
-            HostListener, Input, Output } from '@angular/core';
+    HostListener, Input, Output } from '@angular/core';
+import { FileItem } from 'src/app/models/file-item';
 
 @Directive({
     selector: '[appNgDropFiles]'
 })
     export class NGDropFilesDirective {
 
+        @Input() archivos: FileItem[] = [];
         @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
         constructor() { }
 
@@ -18,6 +20,38 @@ import { Directive, EventEmitter, ElementRef,
         @HostListener('dragleave', ['$event'])
         public onDragLeave( event: any ){
             this.mouseSobre.emit( false );
+            
+        }
+
+        //Validaciones
+        private _archivoPuedeSerCargado( archivo: File ): boolean {
+            if ( !this._archivoYaFueDroppeado( archivo.name ) && this._esImagen( archivo.type ) ){
+                return true;
+            }else {
+                return false;
+            }
+        }
+
+
+        private _prevenirDetener(event) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        private _archivoYaFueDroppeado ( nombreArchivo:string ): boolean{
+
+            for( const archivo of this.archivos ){
+
+                if ( archivo.nombreArchivo === nombreArchivo ){
+                        console.log('El archivo ' + nombreArchivo + 'ya esta agregado');
+                        return true;
+                }
+            }
+                return false;
+        }
+    
+        private _esImagen( tipoArchivo: string ): boolean {
+            return (tipoArchivo === '' || tipoArchivo === undefined ) ? false : tipoArchivo.startsWith('image');
         }
 
     }
